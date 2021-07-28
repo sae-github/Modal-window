57c83afedcbc1231daefb7d5716be88227ed51da
sae-github <miyasae1126@gmail.com>
2021-07-28 21:32:03 +0900

    clean:index.html style.css index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-const openBtn = document.getElementById('js-open-btn');
-const closeBtn = document.getElementById('js-close-btn');
-const overLay = document.getElementById('js-overlay');
-const modal = document.getElementById('js-modal');
+const openBtn = document.getElementById("js-open-btn");
+const closeBtn = document.getElementById("js-close-btn");
+const modal = document.getElementById("js-modal");
 
-openBtn.addEventListener('click',function() {
-  modal.classList.add('is-show');
-  overLay.classList.add('is-show');
-});
-closeBtn.addEventListener('click',function() {
-  modal.classList.remove('is-show');
-  overLay.classList.remove('is-show');
-});
-overLay.addEventListener('click',function() {
-  this.classList.remove('is-show');
-  modal.classList.remove('is-show');
-});
+
+openBtn.onclick = () => {
+  modal.style.opacity = "1";
+  modal.style.zIndex = "1";
+};
+
+closeBtn.onclick = () => {
+  modal.style.opacity = "0";
+  modal.style.zIndex = "-1";
+
+};
+
+window.onclick = function (e) {
+  if (e.target === modal) {
+    modal.style.opacity = "0";
+    modal.style.zIndex = "-1";
+  }
+};
+
